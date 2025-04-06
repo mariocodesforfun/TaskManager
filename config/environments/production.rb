332060b13e187315699c007c87ce0e9c82ebaa8e
mariocodesforfun <gegpriftimario@gmail.com>
2025-04-06 14:15:40 -0400

    Fix: Allow Cloud Run hosts using region-specific regex

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,8 @@ Rails.application.configure do
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
   #
-  config.hosts << /.*\.run\.app/
+  config.hosts << /.*\.us-central1\.run\.app/
+
 
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
